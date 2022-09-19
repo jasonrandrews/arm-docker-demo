a981a8ef9e1467ddc9b2d1f42c65058dd7736497
Jason Andrews <jason.r.andrews@comcast.net>
2022-09-19 14:32:56 -0500

    add manifest example

--- hello-world/join.sh
+++ hello-world/join.sh
@@ -5,7 +5,6 @@ IMG=hello-world
 
 docker manifest create $HUBU/$IMG:latest \
 --amend $HUBU/$IMG:latest-aarch64 \
---amend $HUBU/$IMG:latest-armv7 \
 --amend $HUBU/$IMG:latest-amd64
 docker manifest push --purge $HUBU/$IMG:latest
 
